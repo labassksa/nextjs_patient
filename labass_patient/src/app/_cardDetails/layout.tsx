8532946b9119bc506873a6596ba7730c5e1509a9
YazeedAlbadrani <90254129+YazeedAlbadrani@users.noreply.github.com>
2024-06-23 23:07:28 +0300

    with json

---
+++ labass_patient/src/app/_cardDetails/layout.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  return (
+    <>
+      <script src="https://demo.myfatoorah.com/cardview/v2/session.js" async></script>
+      {children}
+    </>
+  );
+};
+
+export default Layout;
