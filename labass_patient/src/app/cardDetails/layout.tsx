213d57807325b9c2f02a186b07430b7cc7d46cfb
YazeedAlbadrani <90254129+YazeedAlbadrani@users.noreply.github.com>
2024-06-23 22:59:51 +0300

    no json

--- labass_patient/src/app/cardDetails/layout.tsx
+++ labass_patient/src/app/cardDetails/layout.tsx
@@ -1,25 +1,12 @@
-// app/cardDetails/layout.tsx
-import React, { ReactNode } from 'react';
+// import React from "react";
 
-interface CardDetailsLayoutProps {
-  children: ReactNode;
-}
+// const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+//   return (
+//     <>
+//       <script src="https://demo.myfatoorah.com/cardview/v2/session.js"></script>
+//       {children}
+//     </>
+//   );
+// };
 
-const CardDetailsLayout: React.FC<CardDetailsLayoutProps> = ({ children }) => {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="UTF-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Card View Payment</title>
-        <script src="https://demo.myfatoorah.com/cardview/v2/session.js"></script>
-      </head>
-      <body>
-        {children}
-      </body>
-    </html>
-  );
-};
-
-export default CardDetailsLayout;
+// export default Layout;
