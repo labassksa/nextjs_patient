2aa68c4444f39ec873508e59bce82fef4af10d0e
YazeedAlbadrani <90254129+YazeedAlbadrani@users.noreply.github.com>
2024-09-09 13:00:39 +0300

    payment redirect

---
+++ labass_patient/src/app/cardDetails/success/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function ErrorPage() {
+  return (
+    <div>
+      <h1>Payment Success</h1>
+      <p>Payment Success</p>
+      <p>Payment Success.</p>
+    </div>
+  );
+}
