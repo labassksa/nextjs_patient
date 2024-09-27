7949656161f5280a09b3ee861fe39b181073a585
YazeedAlbadrani <90254129+YazeedAlbadrani@users.noreply.github.com>
2024-09-27 15:20:24 +0300

    bg gray

--- labass_patient/src/app/chat/[consultationId]/page.tsx
+++ labass_patient/src/app/chat/[consultationId]/page.tsx
@@ -170,7 +170,7 @@ const ChatPage: React.FC = () => {
   }
 
   return (
-    <div className="flex flex-col min-h-screen bg-red-100">
+    <div className="flex flex-col min-h-screen bg-gray-100">
       {/* Fixed header */}
       <div className="sticky top-0 w-full bg-white z-50">
         <Header title="استشارة فورية" showBackButton={true} />
