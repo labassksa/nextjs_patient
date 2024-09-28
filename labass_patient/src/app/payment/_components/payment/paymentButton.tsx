9a9232a481fc914289bf508f1b7c6ba03014d1f2
YazeedAlbadrani <90254129+YazeedAlbadrani@users.noreply.github.com>
2024-09-28 09:10:32 +0300

    push to consultation chat

--- labass_patient/src/app/payment/_components/payment/paymentButton.tsx
+++ labass_patient/src/app/payment/_components/payment/paymentButton.tsx
@@ -221,7 +221,7 @@ const PaymentButton: React.FC<PaymentButtonProps> = ({
         const paymentUrl = response.data.Data.PaymentURL;
 
         const paymentResponse = await axios.get(paymentUrl);
-        const consultationId = response.data.consultation;
+        const consultationId = response.data.consultation.id;
         // Push to the chat page with consultationId in the URL
         router.push(`/chat/${consultationId}`);
 
