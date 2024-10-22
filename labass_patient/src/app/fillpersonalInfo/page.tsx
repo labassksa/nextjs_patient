a76275c8943a5b09d3a0298fec8d19ba24f7d624
YazeedAlbadrani <90254129+YazeedAlbadrani@users.noreply.github.com>
2024-10-22 19:19:39 +0300

    update again

--- labass_patient/src/app/fillpersonalInfo/page.tsx
+++ labass_patient/src/app/fillpersonalInfo/page.tsx
@@ -1,19 +1,25 @@
 "use client";
-import React from "react";
+import React, { Suspense } from "react";
 import Header from "../../components/common/header";
 import PersonalInfoForm from "./_components/userInformation/form";
+import Spinner from "./_components/spinner"; // Import the Spinner component
 
 const PersonalInfo = () => {
   return (
     <div className="flex flex-col bg-gray-100 min-h-screen">
       <Header title="أدخل المعلومات الشخصية" showBackButton />
       <div className="pt-16 w-full">
-        {" "}
-        {/* Adjust this padding to match your header height */}
         <PersonalInfoForm />
       </div>
     </div>
   );
 };
 
-export default PersonalInfo;
+// Wrap in Suspense to handle any client-side fetching and ensure the `useSearchParams` (if used inside `PersonalInfoForm`) works properly.
+export default function SuspenseWrapper() {
+  return (
+    <Suspense fallback={<Spinner />}>
+      <PersonalInfo />
+    </Suspense>
+  );
+}
