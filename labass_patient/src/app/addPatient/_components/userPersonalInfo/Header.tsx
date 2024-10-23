07bf2ccff6e448dabe2302aa356a7bc8cb74fb96
YazeedAlbadrani <90254129+YazeedAlbadrani@users.noreply.github.com>
2024-10-24 00:22:47 +0300

    Add patient

--- labass_patient/src/app/addPatient/_components/userPersonalInfo/Header.tsx
+++ labass_patient/src/app/addPatient/_components/userPersonalInfo/Header.tsx
@@ -9,7 +9,7 @@ const PersonalInfoHeader: React.FC = () => {
   return (
     <div className="fixed top-0 w-full bg-white p-4 flex items-center justify-between">
       <h1 className="text-lg text-black font-normal flex-grow text-center">
-        المعلومات الشخصية
+        إضافة مريض{" "}
       </h1>
       <button onClick={() => router.back()} className="mr-2">
         <ChevronRightIcon className="h-5 w-5 text-black" aria-hidden="true" />
