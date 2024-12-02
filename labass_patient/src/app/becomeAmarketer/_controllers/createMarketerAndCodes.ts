cd9c5b7cbd3e60c543fca2e2698b8b068e33c5fa
YazeedAlbadrani <90254129+YazeedAlbadrani@users.noreply.github.com>
2024-12-02 19:40:48 +0300

    validation

---
+++ labass_patient/src/app/becomeAmarketer/_controllers/createMarketerAndCodes.ts
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+export const createMarketerAndGeneratePromoCodes = async (
+  name: string,
+  phoneNumber: string,
+  promoterName: string
+) => {
+  try {
+    const formattedPhoneNumber = phoneNumber.startsWith("+966")
+      ? phoneNumber
+      : `+966${phoneNumber}`;
+    const data = {
+      name,
+      phoneNumber: formattedPhoneNumber,
+      promoterName,
+    };
+
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+
+    const response = await axios.post(
+      `${apiUrl}/createMarketerandGeneratePromoCodes`,
+      data
+    );
+
+    if (response.status === 200) {
+      return { success: true, data: response.data };
+    } else {
+      return { success: false, message: "Unexpected response status code" };
+    }
+  } catch (error) {
+    return {
+      success: false,
+      message:
+        axios.isAxiosError(error) && error.response?.data?.error
+          ? error.response.data.error
+          : "حدث خطأ ، حاول مرة أخرى",
+    };
+  }
+};
