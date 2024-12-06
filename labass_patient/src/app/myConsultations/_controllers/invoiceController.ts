6c9e32178d63a5c2b63c1b77c223272bdd21fdc2
YazeedAlbadrani <90254129+YazeedAlbadrani@users.noreply.github.com>
2024-12-06 17:00:12 +0300

    invoicing

--- labass_patient/src/app/myConsultations/_controllers/invoiceController.ts
+++ labass_patient/src/app/myConsultations/_controllers/invoiceController.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiUrl = "process.env.NEXT_PUBLIC_API_URL";
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
 export const fetchInvoiceLink = async (
   consultationId: number
