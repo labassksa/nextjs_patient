557a2d34960251d0697d78cc229e4f8a87efc1e5
Mostafa Yassin <mostafayassin@Mostafas-MacBook-Air.local>
2026-08-03 19:09:45 +0300

    hide wallet tab for now

--- labass_patient/src/app/orgPortal/_components/bottomNavBar.tsx
+++ labass_patient/src/app/orgPortal/_components/bottomNavBar.tsx
@@ -5,7 +5,6 @@ import {
   Assignment as PatientsIcon,
   PersonAdd as RegistrationIcon,
   CardMembership as SubscriptionIcon,
-  AccountBalanceWalletOutlined as WalletIcon,
 } from "@mui/icons-material";
 import { useTranslation } from "react-i18next";
 
@@ -50,15 +49,6 @@ const LabBottomNavBar: React.FC<LabBottomNavBarProps> = ({
       >
         <SubscriptionIcon fontSize="small" />
         <span>{t('subscription.tabLabel')}</span>
-      </button>
-      <button
-        className={`flex flex-col items-center ${
-          currentView === "wallet" ? "text-blue-500" : "text-gray-500"
-        }`}
-        onClick={() => onToggleView("wallet")}
-      >
-        <WalletIcon fontSize="small" />
-        <span>{t('wallet.tabLabel')}</span>
       </button>
     </nav>
   );
